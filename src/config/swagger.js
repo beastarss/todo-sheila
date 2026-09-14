56c2e50c442c27c12c6d987b891595fedf4e8245
sheilaaulinda <sheilaaulinda@gmail.com>
2026-09-14 09:23:22 +0700

    fix swagger apis pattern for routes files

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -83,7 +83,7 @@ const options = {
   },
 
   // Diubah menggunakan path.join dan __dirname agar aman di Vercel
-  apis: [path.join(__dirname, "../routes/*.js")],
+ apis: [path.join(__dirname, "./*.routes.js"), path.join(__dirname, "../routes/*.routes.js")],
 };
 
 const swaggerSpec = swaggerJSDoc(options);
