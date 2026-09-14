1a562d898a2931846ec8762c897ca2c4d74d9146
sheilaaulinda <sheilaaulinda@gmail.com>
2026-09-14 13:32:40 +0700

    ignore favicon.ico in notFound middleware

--- src/middlewares/notFound.middleware.js
+++ src/middlewares/notFound.middleware.js
@@ -1,6 +1,10 @@
 const AppError = require("../utils/AppError");
 
 function notFound(req, res, next) {
+  // Abaikan request otomatis untuk favicon agar tidak memicu error 404/500 di log
+  if (req.originalUrl === '/favicon.ico') {
+    return res.status(204).end();
+  }
   next(new AppError(`Route ${req.method} ${req.originalUrl} not found`, 404));
 }
 
